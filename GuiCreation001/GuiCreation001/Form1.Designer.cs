14e9ac26c009a636ad2c8f058670ce4cef217b26
Benjamin Kleynhans <kleynhansbenjamin@gmail.com>
2018-01-29 09:46:24 -0500

    Changed names of all objects to start with lower aase

--- GuiCreation001/GuiCreation001/Form1.Designer.cs
+++ GuiCreation001/GuiCreation001/Form1.Designer.cs
@@ -30,204 +30,201 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.LblRulesTitle = new System.Windows.Forms.Label();
-            this.LblRulesRules = new System.Windows.Forms.Label();
-            this.ContRules = new System.Windows.Forms.GroupBox();
-            this.ContWires = new System.Windows.Forms.GroupBox();
-            this.BtnWire004 = new System.Windows.Forms.Button();
-            this.BtnWire005 = new System.Windows.Forms.Button();
-            this.BtnWire001 = new System.Windows.Forms.Button();
-            this.BtnWire002 = new System.Windows.Forms.Button();
-            this.BtnWire003 = new System.Windows.Forms.Button();
-            this.BtnStartStopReset = new System.Windows.Forms.Button();
-            this.ProgressBar001 = new System.Windows.Forms.ProgressBar();
+            this.lblRulesTitle = new System.Windows.Forms.Label();
+            this.lblRulesRules = new System.Windows.Forms.Label();
+            this.contRules = new System.Windows.Forms.GroupBox();
+            this.contWires = new System.Windows.Forms.GroupBox();
+            this.btnWire004 = new System.Windows.Forms.Button();
+            this.btnWire005 = new System.Windows.Forms.Button();
+            this.btnWire001 = new System.Windows.Forms.Button();
+            this.btnWire002 = new System.Windows.Forms.Button();
+            this.btnWire003 = new System.Windows.Forms.Button();
+            this.btnStartStopReset = new System.Windows.Forms.Button();
+            this.progressBar001 = new System.Windows.Forms.ProgressBar();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.ContStatus = new System.Windows.Forms.GroupBox();
-            this.LblStatusTitle = new System.Windows.Forms.Label();
-            this.CmbStatus = new System.Windows.Forms.ListBox();
-            this.ContRules.SuspendLayout();
-            this.ContWires.SuspendLayout();
-            this.ContStatus.SuspendLayout();
+            this.contStatus = new System.Windows.Forms.GroupBox();
+            this.cmbBStatus = new System.Windows.Forms.ListBox();
+            this.lblStatusTitle = new System.Windows.Forms.Label();
+            this.contRules.SuspendLayout();
+            this.contWires.SuspendLayout();
+            this.contStatus.SuspendLayout();
             this.SuspendLayout();
             // 
-            // LblRulesTitle
+            // lblRulesTitle
             // 
-            this.LblRulesTitle.AutoSize = true;
-            this.LblRulesTitle.Location = new System.Drawing.Point(10, 30);
-            this.LblRulesTitle.Name = "LblRulesTitle";
-            this.LblRulesTitle.Size = new System.Drawing.Size(44, 17);
-            this.LblRulesTitle.TabIndex = 0;
-            this.LblRulesTitle.Text = "Rules";
+            this.lblRulesTitle.AutoSize = true;
+            this.lblRulesTitle.Location = new System.Drawing.Point(10, 30);
+            this.lblRulesTitle.Name = "lblRulesTitle";
+            this.lblRulesTitle.Size = new System.Drawing.Size(44, 17);
+            this.lblRulesTitle.TabIndex = 0;
+            this.lblRulesTitle.Text = "Rules";
             // 
-            // LblRulesRules
+            // lblRulesRules
             // 
-            this.LblRulesRules.AutoSize = true;
-            this.LblRulesRules.Location = new System.Drawing.Point(20, 59);
-            this.LblRulesRules.Name = "LblRulesRules";
-            this.LblRulesRules.Size = new System.Drawing.Size(443, 68);
-            this.LblRulesRules.TabIndex = 1;
-            this.LblRulesRules.Text = resources.GetString("LblRulesRules.Text");
-            this.LblRulesRules.Click += new System.EventHandler(this.LblRulesRules_Click);
+            this.lblRulesRules.AutoSize = true;
+            this.lblRulesRules.Location = new System.Drawing.Point(20, 59);
+            this.lblRulesRules.Name = "lblRulesRules";
+            this.lblRulesRules.Size = new System.Drawing.Size(443, 68);
+            this.lblRulesRules.TabIndex = 1;
+            this.lblRulesRules.Text = resources.GetString("lblRulesRules.Text");
+            this.lblRulesRules.Click += new System.EventHandler(this.LblRulesRules_Click);
             // 
-            // ContRules
+            // contRules
             // 
-            this.ContRules.Controls.Add(this.LblRulesRules);
-            this.ContRules.Controls.Add(this.LblRulesTitle);
-            this.ContRules.Location = new System.Drawing.Point(21, 29);
-            this.ContRules.Name = "ContRules";
-            this.ContRules.Size = new System.Drawing.Size(510, 136);
-            this.ContRules.TabIndex = 2;
-            this.ContRules.TabStop = false;
-            this.ContRules.Text = "ContRules";
+            this.contRules.Controls.Add(this.lblRulesRules);
+            this.contRules.Controls.Add(this.lblRulesTitle);
+            this.contRules.Location = new System.Drawing.Point(21, 29);
+            this.contRules.Name = "contRules";
+            this.contRules.Size = new System.Drawing.Size(510, 136);
+            this.contRules.TabIndex = 2;
+            this.contRules.TabStop = false;
             // 
-            // ContWires
+            // contWires
             // 
-            this.ContWires.Controls.Add(this.BtnWire004);
-            this.ContWires.Controls.Add(this.BtnWire005);
-            this.ContWires.Controls.Add(this.BtnWire001);
-            this.ContWires.Controls.Add(this.BtnWire002);
-            this.ContWires.Controls.Add(this.BtnWire003);
-            this.ContWires.Location = new System.Drawing.Point(21, 201);
-            this.ContWires.Name = "ContWires";
-            this.ContWires.Size = new System.Drawing.Size(510, 133);
-            this.ContWires.TabIndex = 8;
-            this.ContWires.TabStop = false;
-            this.ContWires.Text = "ContWires";
+            this.contWires.Controls.Add(this.btnWire004);
+            this.contWires.Controls.Add(this.btnWire005);
+            this.contWires.Controls.Add(this.btnWire001);
+            this.contWires.Controls.Add(this.btnWire002);
+            this.contWires.Controls.Add(this.btnWire003);
+            this.contWires.Location = new System.Drawing.Point(21, 201);
+            this.contWires.Name = "contWires";
+            this.contWires.Size = new System.Drawing.Size(510, 133);
+            this.contWires.TabIndex = 8;
+            this.contWires.TabStop = false;
             // 
-            // BtnWire004
+            // btnWire004
             // 
-            this.BtnWire004.BackColor = System.Drawing.Color.White;
-            this.BtnWire004.Enabled = false;
-            this.BtnWire004.Location = new System.Drawing.Point(51, 82);
-            this.BtnWire004.Name = "BtnWire004";
-            this.BtnWire004.Size = new System.Drawing.Size(414, 10);
-            this.BtnWire004.TabIndex = 11;
-            this.BtnWire004.UseVisualStyleBackColor = false;
+            this.btnWire004.BackColor = System.Drawing.Color.White;
+            this.btnWire004.Enabled = false;
+            this.btnWire004.Location = new System.Drawing.Point(51, 82);
+            this.btnWire004.Name = "btnWire004";
+            this.btnWire004.Size = new System.Drawing.Size(414, 10);
+            this.btnWire004.TabIndex = 11;
+            this.btnWire004.UseVisualStyleBackColor = false;
             // 
-            // BtnWire005
+            // btnWire005
             // 
-            this.BtnWire005.BackColor = System.Drawing.Color.Yellow;
-            this.BtnWire005.Enabled = false;
-            this.BtnWire005.Location = new System.Drawing.Point(51, 98);
-            this.BtnWire005.Name = "BtnWire005";
-            this.BtnWire005.Size = new System.Drawing.Size(414, 10);
-            this.BtnWire005.TabIndex = 12;
-            this.BtnWire005.UseVisualStyleBackColor = false;
+            this.btnWire005.BackColor = System.Drawing.Color.Yellow;
+            this.btnWire005.Enabled = false;
+            this.btnWire005.Location = new System.Drawing.Point(51, 98);
+            this.btnWire005.Name = "btnWire005";
+            this.btnWire005.Size = new System.Drawing.Size(414, 10);
+            this.btnWire005.TabIndex = 12;
+            this.btnWire005.UseVisualStyleBackColor = false;
             // 
-            // BtnWire001
+            // btnWire001
             // 
-            this.BtnWire001.BackColor = System.Drawing.Color.Red;
-            this.BtnWire001.Enabled = false;
-            this.BtnWire001.Location = new System.Drawing.Point(51, 34);
-            this.BtnWire001.Name = "BtnWire001";
-            this.BtnWire001.Size = new System.Drawing.Size(414, 10);
-            this.BtnWire001.TabIndex = 8;
-            this.BtnWire001.UseVisualStyleBackColor = false;
+            this.btnWire001.BackColor = System.Drawing.Color.Red;
+            this.btnWire001.Enabled = false;
+            this.btnWire001.Location = new System.Drawing.Point(51, 34);
+            this.btnWire001.Name = "btnWire001";
+            this.btnWire001.Size = new System.Drawing.Size(414, 10);
+            this.btnWire001.TabIndex = 8;
+            this.btnWire001.UseVisualStyleBackColor = false;
             // 
-            // BtnWire002
+            // btnWire002
             // 
-            this.BtnWire002.BackColor = System.Drawing.Color.Black;
-            this.BtnWire002.Enabled = false;
-            this.BtnWire002.Location = new System.Drawing.Point(51, 50);
-            this.BtnWire002.Name = "BtnWire002";
-            this.BtnWire002.Size = new System.Drawing.Size(414, 10);
-            this.BtnWire002.TabIndex = 9;
-            this.BtnWire002.UseVisualStyleBackColor = false;
+            this.btnWire002.BackColor = System.Drawing.Color.Black;
+            this.btnWire002.Enabled = false;
+            this.btnWire002.Location = new System.Drawing.Point(51, 50);
+            this.btnWire002.Name = "btnWire002";
+            this.btnWire002.Size = new System.Drawing.Size(414, 10);
+            this.btnWire002.TabIndex = 9;
+            this.btnWire002.UseVisualStyleBackColor = false;
             // 
-            // BtnWire003
+            // btnWire003
             // 
-            this.BtnWire003.BackColor = System.Drawing.Color.Blue;
-            this.BtnWire003.Enabled = false;
-            this.BtnWire003.Location = new System.Drawing.Point(51, 66);
-            this.BtnWire003.Name = "BtnWire003";
-            this.BtnWire003.Size = new System.Drawing.Size(414, 10);
-            this.BtnWire003.TabIndex = 10;
-            this.BtnWire003.UseVisualStyleBackColor = false;
+            this.btnWire003.BackColor = System.Drawing.Color.Blue;
+            this.btnWire003.Enabled = false;
+            this.btnWire003.Location = new System.Drawing.Point(51, 66);
+            this.btnWire003.Name = "btnWire003";
+            this.btnWire003.Size = new System.Drawing.Size(414, 10);
+            this.btnWire003.TabIndex = 10;
+            this.btnWire003.UseVisualStyleBackColor = false;
             // 
-            // BtnStartStopReset
+            // btnStartStopReset
             // 
-            this.BtnStartStopReset.Location = new System.Drawing.Point(393, 48);
-            this.BtnStartStopReset.Name = "BtnStartStopReset";
-            this.BtnStartStopReset.Size = new System.Drawing.Size(132, 37);
-            this.BtnStartStopReset.TabIndex = 9;
-            this.BtnStartStopReset.Text = "Start Game";
-            this.BtnStartStopReset.UseVisualStyleBackColor = true;
+            this.btnStartStopReset.Location = new System.Drawing.Point(393, 48);
+            this.btnStartStopReset.Name = "btnStartStopReset";
+            this.btnStartStopReset.Size = new System.Drawing.Size(132, 37);
+            this.btnStartStopReset.TabIndex = 9;
+            this.btnStartStopReset.Text = "Start Game";
+            this.btnStartStopReset.UseVisualStyleBackColor = true;
             // 
-            // ProgressBar001
+            // progressBar001
             // 
-            this.ProgressBar001.Location = new System.Drawing.Point(21, 171);
-            this.ProgressBar001.Name = "ProgressBar001";
-            this.ProgressBar001.Size = new System.Drawing.Size(510, 24);
-            this.ProgressBar001.TabIndex = 10;
-            this.ProgressBar001.Click += new System.EventHandler(this.progressBar_Click);
+            this.progressBar001.Location = new System.Drawing.Point(21, 171);
+            this.progressBar001.Name = "progressBar001";
+            this.progressBar001.Size = new System.Drawing.Size(510, 24);
+            this.progressBar001.TabIndex = 10;
+            this.progressBar001.Click += new System.EventHandler(this.progressBar_Click);
             // 
-            // ContStatus
+            // contStatus
             // 
-            this.ContStatus.Controls.Add(this.CmbStatus);
-            this.ContStatus.Controls.Add(this.LblStatusTitle);
-            this.ContStatus.Location = new System.Drawing.Point(21, 340);
-            this.ContStatus.Name = "ContStatus";
-            this.ContStatus.Size = new System.Drawing.Size(510, 164);
-            this.ContStatus.TabIndex = 11;
-            this.ContStatus.TabStop = false;
-            this.ContStatus.Text = "ContStatus";
+            this.contStatus.Controls.Add(this.cmbBStatus);
+            this.contStatus.Controls.Add(this.lblStatusTitle);
+            this.contStatus.Location = new System.Drawing.Point(21, 340);
+            this.contStatus.Name = "contStatus";
+            this.contStatus.Size = new System.Drawing.Size(510, 164);
+            this.contStatus.TabIndex = 11;
+            this.contStatus.TabStop = false;
             // 
-            // LblStatusTitle
+            // cmbBStatus
             // 
-            this.LblStatusTitle.AutoSize = true;
-            this.LblStatusTitle.Location = new System.Drawing.Point(10, 27);
-            this.LblStatusTitle.Name = "LblStatusTitle";
-            this.LblStatusTitle.Size = new System.Drawing.Size(48, 17);
-            this.LblStatusTitle.TabIndex = 13;
-            this.LblStatusTitle.Text = "Status";
+            this.cmbBStatus.FormattingEnabled = true;
+            this.cmbBStatus.ItemHeight = 16;
+            this.cmbBStatus.Location = new System.Drawing.Point(13, 48);
+            this.cmbBStatus.Name = "cmbBStatus";
+            this.cmbBStatus.Size = new System.Drawing.Size(491, 100);
+            this.cmbBStatus.TabIndex = 14;
             // 
-            // CmbStatus
+            // lblStatusTitle
             // 
-            this.CmbStatus.FormattingEnabled = true;
-            this.CmbStatus.ItemHeight = 16;
-            this.CmbStatus.Location = new System.Drawing.Point(13, 48);
-            this.CmbStatus.Name = "CmbStatus";
-            this.CmbStatus.Size = new System.Drawing.Size(491, 100);
-            this.CmbStatus.TabIndex = 14;
+            this.lblStatusTitle.AutoSize = true;
+            this.lblStatusTitle.Location = new System.Drawing.Point(10, 27);
+            this.lblStatusTitle.Name = "lblStatusTitle";
+            this.lblStatusTitle.Size = new System.Drawing.Size(48, 17);
+            this.lblStatusTitle.TabIndex = 13;
+            this.lblStatusTitle.Text = "Status";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(543, 516);
-            this.Controls.Add(this.ContStatus);
-            this.Controls.Add(this.ProgressBar001);
-            this.Controls.Add(this.BtnStartStopReset);
-            this.Controls.Add(this.ContWires);
-            this.Controls.Add(this.ContRules);
+            this.Controls.Add(this.contStatus);
+            this.Controls.Add(this.progressBar001);
+            this.Controls.Add(this.btnStartStopReset);
+            this.Controls.Add(this.contWires);
+            this.Controls.Add(this.contRules);
             this.Name = "Form1";
             this.Text = "Keep Talking and Nobody Explodes";
-            this.ContRules.ResumeLayout(false);
-            this.ContRules.PerformLayout();
-            this.ContWires.ResumeLayout(false);
-            this.ContStatus.ResumeLayout(false);
-            this.ContStatus.PerformLayout();
+            this.contRules.ResumeLayout(false);
+            this.contRules.PerformLayout();
+            this.contWires.ResumeLayout(false);
+            this.contStatus.ResumeLayout(false);
+            this.contStatus.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label LblRulesTitle;
-        private System.Windows.Forms.Label LblRulesRules;
-        private System.Windows.Forms.GroupBox ContRules;
-        private System.Windows.Forms.GroupBox ContWires;
-        private System.Windows.Forms.Button BtnWire004;
-        private System.Windows.Forms.Button BtnWire005;
-        private System.Windows.Forms.Button BtnWire001;
-        private System.Windows.Forms.Button BtnWire002;
-        private System.Windows.Forms.Button BtnWire003;
-        private System.Windows.Forms.Button BtnStartStopReset;
-        private System.Windows.Forms.ProgressBar ProgressBar001;
+        private System.Windows.Forms.Label lblRulesTitle;
+        private System.Windows.Forms.Label lblRulesRules;
+        private System.Windows.Forms.GroupBox contRules;
+        private System.Windows.Forms.GroupBox contWires;
+        private System.Windows.Forms.Button btnWire004;
+        private System.Windows.Forms.Button btnWire005;
+        private System.Windows.Forms.Button btnWire001;
+        private System.Windows.Forms.Button btnWire002;
+        private System.Windows.Forms.Button btnWire003;
+        private System.Windows.Forms.Button btnStartStopReset;
+        private System.Windows.Forms.ProgressBar progressBar001;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.GroupBox ContStatus;
-        private System.Windows.Forms.Label LblStatusTitle;
-        private System.Windows.Forms.ListBox CmbStatus;
+        private System.Windows.Forms.GroupBox contStatus;
+        private System.Windows.Forms.Label lblStatusTitle;
+        private System.Windows.Forms.ListBox cmbBStatus;
     }
 }
 
