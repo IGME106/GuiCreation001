2246b84a629d4b6259309b038ce90e423a0bec3c
Benjamin Kleynhans <kleynhansbenjamin@gmail.com>
2018-01-30 16:21:01 -0500

    Added comments to code

--- GuiCreation001/GuiCreation001/Form1.cs
+++ GuiCreation001/GuiCreation001/Form1.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+/// <summary>
+/// IGME-106 - Game Development and Algorithmic Problem Solving
+/// Practice Exercise 5 - UI Events
+/// Class Description   : This is the Form1 class for Practice Exercise 5
+/// Author              : Benjamin Kleynhans
+/// Modified By         : Benjamin Kleynhans
+/// Date                : January 30, 2018
+/// Filename            : Form1.cs
+/// </summary>
 
 namespace GuiCreation001 {
     public partial class Form1 : Form {
+        /// <summary>
+        /// Define list of colors to be used for dynamic wire allocation
+        /// </summary>
         private static List<Color> wireColor = new List<Color> {
             Color.Red,
             Color.Black,
@@ -18,10 +26,14 @@ namespace GuiCreation001 {
             Color.Yellow
         };
 
-        private static List<Button> buttonList = new List<Button> { };
+        private static List<Button> buttonList = new List<Button> { };      // List of button objects
 
-        private static Random random = new Random();                       // Instantiate a random number generator
+        private static Random random = new Random();                        // Instantiate a random number generator
 
+        /// <summary>
+        /// The following variable values are calculated to assist in the
+        ///     validation of the game rules.
+        /// </summary>
         int redWires = 0;
         bool lastWireWhite = false;
         int blueWires = 0;
@@ -31,6 +43,11 @@ namespace GuiCreation001 {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Create an array of buttons (by name) to attach to the list
+        /// </summary>
+        /// <param name="sender">Object sending the event</param>
+        /// <param name="e">Event details</param>
         private void Form1_Load(object sender, EventArgs e) {
 
             Button[] formButtons = {btnWire001,
@@ -42,80 +59,87 @@ namespace GuiCreation001 {
             buttonList.AddRange(formButtons);
         }
 
+        /// <summary>
+        /// Configure progress bar to run as countdown timer
+        /// </summary>
+        /// <param name="sender">Object sending the event</param>
+        /// <param name="e">Event details</param>
         private void timer1_Tick(object sender, EventArgs e) {
 
             if (this.progressBar001.Value < 100)
             {
-                this.progressBar001.Increment(1);
+                this.progressBar001.Increment(1);                   // Increase progress bar by one every second
             }
             else
             {
-                StartResetGame("StopGame");
-//                this.btnStartReset.Enabled = true;
+                StartResetGame("StopGame");                         // Stop the game when time runs out
             }
         }
-
+        /// <summary>
+        /// Set the color of the button on "mouse-over (MouseEnter)" event
+        /// </summary>
+        /// <param name="sender">Object sending the event</param>
+        /// <param name="e">Event details</param>
         private void btnStartReset_MouseEnter(object sender, EventArgs e) {
 
             btnStartReset.BackColor = Color.LightGray;
         }
 
+        /// <summary>        
+        /// Button click event indicating what game operation to do based on the 
+        ///     text of the button.
+        /// </summary>
+        /// <param name="sender">Object sending the event</param>
+        /// <param name="e">Event details</param>
         private void btnStartReset_Click(object sender, EventArgs e) {
 
             if (btnStartReset.Text.Equals("Start Game"))
             {
                 StartResetGame("StartGame");
-                //btnStartReset.Text = "Reset Game";
-                //btnStartReset.Enabled = false;
-                //changeWireState("enable");
-
-                //SetWireColors();
-                //GetBombProperties();
-
-                //timer1.Start();
             }
             else
             {
                 StartResetGame("ResetGame");
-                //btnStartReset.Text = "Start Game";
-                //progressBar001.Value = 0;
-                //changeWireState("disable");
-
-                //timer1.Stop();
             }
         }
 
+        /// <summary>
+        /// Start or reset the game based on input
+        /// </summary>
+        /// <param name="stateRequest">Calling method specifies the required action to take</param>
         private void StartResetGame (string stateRequest) {
 
+            // Performed the specified list of functions based on the parameter passed in
+            //  from the calling method
             switch (stateRequest)
             {
                 case "StartGame":
                     btnStartReset.Text = "Reset Game";
                     btnStartReset.Enabled = false;
-                    changeWireState("enable");
+                    changeWireState("enable");                      // Enable the wires
 
-                    SetWireColors();
-                    GetBombProperties();
+                    SetWireColors();                                // Generate and set random wire colors
+                    GetBombProperties();                            // Get properties (wire colors) as applied
 
-                    timer1.Start();
+                    timer1.Start();                                 // Start the timer
 
                     break;
                 case "StopGame":
                     this.btnStartReset.Enabled = true;
-                    changeWireState("disable");
+                    changeWireState("disable");                     // Disable the wires
 
-                    timer1.Stop();
+                    timer1.Stop();                                  // Stop the timer
 
                     break;
                 case "ResetGame":
                     btnStartReset.Text = "Start Game";
                     this.btnStartReset.Enabled = true;
-                    progressBar001.Value = 0;
-                    changeWireState("disable");
-                    ResetWireColors();
-                    listBStatus.Items.Clear();
+                    progressBar001.Value = 0;                       // Reset the progress bar
+                    changeWireState("disable");                     // Disable the wires
+                    ResetWireColors();                              // Clear wire colors
+                    listBStatus.Items.Clear();                      // Clear the contents of the status box
 
-                    timer1.Stop();
+                    timer1.Stop();                                  // Stop the timer
 
                     break;
                 default:
@@ -123,6 +147,9 @@ namespace GuiCreation001 {
             }
         }
 
+        /// <summary>
+        /// Extracts the associated properties from the bomb.
+        /// </summary>
         private void GetBombProperties() {
 
             redWires = CountRedWires();
@@ -130,36 +157,46 @@ namespace GuiCreation001 {
             blueWires = CountBlueWires();
         }
 
+        /// <summary>
+        /// Determine which wire (button) was clicked
+        /// </summary>
+        /// <param name="sender">Object sending the event</param>
+        /// <param name="e">Event details</param>
         private void btnWire_Click(object sender, EventArgs e) {
 
             Button pressedButton = (Button)sender;
 
             bool winOrLoose = BombLogic(pressedButton);
 
-            if (winOrLoose)
-            {
+            if (winOrLoose)                                         // If the game logic was successfully
+            {                                                       //  followed, you win
                 Console.WriteLine(pressedButton.Name);
                 listBStatus.Items.Add("You Win");
             }
-            else
+            else                                                    // alternatively you loose.
             {
                 Console.WriteLine(pressedButton.Name);
                 listBStatus.Items.Add("You Loose");
             }
 
-            StartResetGame("StopGame");
+            StartResetGame("StopGame");                             // Stop the game once a result is found
         }
 
+        /// <summary>
+        /// Logic for defusing the bomb as specified in the blueprint.
+        /// </summary>
+        /// <param name="buttonClicked">reference to Button object that was clicked</param>
+        /// <returns>True or False.  True if you win the game, false if you loose.</returns>
         private bool BombLogic(Button buttonClicked) {
 
-            bool returnValue = false;
+            bool returnValue = false;                               // Default returnValue is false
 
-            if ((redWires == 0) && 
+            if ((redWires == 0) &&      // If there are no red wires, cut the second wire
                 (buttonClicked.Equals(buttonList[1])))
             {
                 returnValue = true;
             }
-            else if (
+            else if (                   // Otherwise, if the last wire is white, cut the last wire
                     redWires > 0 && 
                     lastWireWhite &&
                     buttonClicked.Equals(buttonList[4])
@@ -167,7 +204,7 @@ namespace GuiCreation001 {
             {
                 returnValue = true;
             }
-            else if (
+            else if (                   // Otherwise, if there is more than one blue wire, cut the last blue wire
                     redWires > 0 &&
                     !lastWireWhite && 
                     blueWires > 1 && 
@@ -176,7 +213,7 @@ namespace GuiCreation001 {
             {
                 returnValue = true;
             }
-            else if (
+            else if (                   // Otherwise, cut the last wire
                     redWires > 0 &&
                     !lastWireWhite &&
                     blueWires <= 1 &&
@@ -186,9 +223,13 @@ namespace GuiCreation001 {
                 returnValue = true;
             }            
 
-            return returnValue;
+            return returnValue;         // Returns True or False based on logic outcome
         }
 
+        /// <summary>
+        /// Swap the wire states between enabled and disabled based on request
+        /// </summary>
+        /// <param name="statusRequest">"enable" or "disable" depending on requirement</param>
         private void changeWireState(String statusRequest) {
 
             switch (statusRequest)
@@ -214,9 +255,13 @@ namespace GuiCreation001 {
             }
         }
 
+        /// <summary>
+        /// Counts the number of red wires that were generated
+        /// </summary>
+        /// <returns>Number or red wires</returns>
         private int CountRedWires() {
 
-            int returnValue = 0;
+            int returnValue = 0;                                    // Default return value is 0
 
             for (int i = 0; i < 5; i++)
             {
@@ -227,16 +272,24 @@ namespace GuiCreation001 {
             return returnValue;
         }
 
+        /// <summary>
+        /// Determines if last wire is white
+        /// </summary>
+        /// <returns>true if wire is white, false if wire is not white</returns>
         private bool LastWireWhite() {
 
-            bool returnValue = false;
+            bool returnValue = false;                               // Default returnValue is false
 
             if (buttonList[4].BackColor == Color.White)
                 returnValue = true;
 
             return returnValue;
         }
-
+        
+        /// <summary>
+        /// Count the number of blue wires allocated
+        /// </summary>
+        /// <returns>the number of blue wires</returns>
         private int CountBlueWires() {
 
             int returnValue = 0;
@@ -246,13 +299,16 @@ namespace GuiCreation001 {
                 if (buttonList[i].BackColor == Color.Blue)
                 {
                     returnValue++;
-                    lastBlueWireIndex = i;
+                    lastBlueWireIndex = i;                          // Determine the index of the last blue wire in the bomb
                 }
             }
 
             return returnValue;
         }
 
+        /// <summary>
+        /// Reset the wires to a default gray background
+        /// </summary>
         private void ResetWireColors() {
 
             for (int i = 0; i < 5; i++)
@@ -261,6 +317,9 @@ namespace GuiCreation001 {
             }
         }
 
+        /// <summary>
+        /// Randomly assign numbers to the wires
+        /// </summary>
         private void SetWireColors() {
 
             for (int i = 0; i < 5; i++)
@@ -269,6 +328,14 @@ namespace GuiCreation001 {
             }
         }
 
+        /// <summary>
+        /// Random number generator.  Generates a number between min and max specified values
+        /// </summary>
+        /// <param name="min">lowest value to include in random number generation</param>
+        /// <param name="max">highest value to include in random number generation.  Please note, 
+        ///                     the max number is always (max - 1), so if 5 is specified, the
+        ///                     highest number that will be generated is 4.</param>
+        /// <returns>A number between 0 and 5 (including 0 but not including 5)</returns>
         private static int RandomNumber(int min, int max) {
             return random.Next(min, max);
         }
--- GuiCreation001/GuiCreation001/Program.cs
+++ GuiCreation001/GuiCreation001/Program.cs
@@ -1,8 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+/// <summary>
+/// IGME-106 - Game Development and Algorithmic Problem Solving
+/// Practice Exercise 5 - UI Events
+/// Class Description   : This is the Program class for Practice Exercise 5
+/// Author              : Benjamin Kleynhans
+/// Modified By         : Benjamin Kleynhans
+/// Date                : January 30, 2018
+/// Filename            : Program.cs
+/// </summary>
 
 namespace GuiCreation001
 {
